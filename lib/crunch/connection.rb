6bfdf6db944052ac8a772ed14a6bf016a603b286
Stephen Eley <sfeley@gmail.com>
2010-07-24 02:05:59 -0400

    Connection passes back correct data, yay

--- lib/crunch/connection.rb
+++ lib/crunch/connection.rb
@@ -3,7 +3,23 @@ module Crunch
   # Passed to EventMachine to handle data received on the client connection.
   # @see http://eventmachine.rubyforge.org/EventMachine.html#M000473
   module Connection
-    def receive_data(data)
+    def initialize(*args)
+      @database = args.shift
+      super
     end
+    
+    def receive_data(data)
+      # Algorithm shamelessly lifted from EventMachine's ObjectProtocol code.
+      (@buffer ||= ''.encode(Encoding::BINARY)) << data
+      @size ||= @buffer.unpack('V')[0]  # This is nil if we don't have 4 bytes yet
+      
+      while @size && @buffer.bytesize >= @size
+        # We have a complete message!  Send it and take it off the front of the buffer.
+        @database.receive_reply(@buffer.slice!(0..@size-1))
+        @size = @buffer.unpack('V')[0]  # Rinse, repeat.
+      end
+    end
+    
   end
+  
 end
