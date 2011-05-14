857045468f5b30a2b711d926a3d399778b9e9fde
Stephen Eley <sfeley@gmail.com>
2011-05-14 16:18:48 -0400

    Got Collection#insert working

--- lib/crunch/collection.rb
+++ lib/crunch/collection.rb
@@ -26,8 +26,35 @@ module Crunch
       @name = name
     end
     
+    # The fully qualified name of the collection, i.e. "database.name"
+    def collection_name
+      @collection_name ||= "#{database.name}.#{name}"
+    end
+    
     def inspect
       "«#{name}»"
+    end
+    
+    # Given a hash or Fieldset, or array of same, sends them to the 
+    # database as an Insert operation.  Returns the '_id' field of the 
+    # document to be inserted, or an array of '_id' fields.  Documents
+    # without '_id' fields will have BSON::ObjectID values generated.
+    #
+    # @param [Hash, Fieldset] *docs One or more hashes or fieldsets
+    def insert(*docs)
+      fieldsets = docs.map do |doc|
+        # Add an _id if there isn't one
+        doc = doc.merge({'_id' => Crunch.oid}) unless doc['_id'] || doc[:_id]
+        Fieldset.new(doc)
+      end
+      
+      database << InsertRequest.new(self, fieldsets)
+      
+      if fieldsets.count == 1
+        fieldsets.first['_id']
+      else
+        fieldsets.map{|f| f['_id']}
+      end
     end
     
   end
--- spec/crunch/collection_spec.rb
+++ spec/crunch/collection_spec.rb
@@ -19,6 +19,10 @@ module Crunch
       @this.name.should == 'TestCollection'
     end
     
+    it "knows its collection name" do
+      @this.collection_name.should == 'crunch_test.TestCollection'
+    end
+    
     it "has an implicit query" do
       pending
     end
@@ -29,16 +33,51 @@ module Crunch
     
     describe "inserting" do
       it "can insert a single document" do
-        @this.insert 'foo' => 'bar'
+        @this.insert 'foo' => 'bar', eleven: 11
         sleep 0.2
-        verifier.find_one('foo' => 'bar').count.should == 1
+        verifier.find_one('foo' => 'bar')['eleven'].should == 11
       end
       
-      it "returns the _id"
+      it "can take a fieldset for a document" do
+        @this.insert Fieldset.new('three' => 3, :five => 5.0)
+        sleep 0.2
+        verifier.find_one('three' => 3)['five'].should == 5.0
+      end
+        
+      it "returns the _id" do
+        id = @this.insert(too: 'tar')
+        sleep 0.2
+        verifier.find_one('too' => 'tar')['_id'].to_s.should == id.to_s
+      end
       
-      it "can insert multiple documents"
+      it "creates an ObjectID if no _id" do
+        id = @this.insert(moo: :mar)
+        id.should be_a(BSON::ObjectID)
+        id.timestamp.should be_within(1).of(Time.now)
+      end
+        
       
-      it "returns an _id array on multiple inserts"
+      it "leaves the _id intact if one was provided" do
+        id = Time.now.to_i
+        @this.insert(woo: :war, '_id' => id).should == id
+        sleep 0.2
+        verifier.find_one('woo' => 'war')['_id'].should == id
+      end
+      
+      it "can insert multiple documents" do
+        id = Time.now.to_f
+        @this.insert({qoo: 'qar'}, {qoo: :qas, _id: id}, {qoo: 7})
+        sleep 0.2
+        verifier.find('qoo' => {'$exists' => 1}).count.should == 3
+      end
+      
+      it "returns an _id array on multiple inserts" do
+        @this.insert({qoo: 'qar', '_id' => 1}, 
+          {qoo: :qas, _id: 2}, {qoo: 7, _id: 3}).should == [1, 2, 3]
+        sleep 0.2
+        verifier.find('_id' => {'$in' => [1, 2, 3]}).count.should == 3
+      end
+        
     end
     
     describe "deleting" do
